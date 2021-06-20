fde3b5162186571832694ae511c5261f402e7437
Pietro Ballarin <pietroballarin@outlook.com>
2021-06-20 16:20:25 +0200

    summary done

--- src/components/PriceOverview.js
+++ src/components/PriceOverview.js
@@ -3,25 +3,49 @@ import axios from 'axios';
 
 export default function PriceOverview(props) {
 
+    const [pricingInfo, setPricingInfo] = useState([])
+
     useEffect(() => {
         axios.get('https://cloud-storage-prices-moberries.herokuapp.com/prices')
         .then(response => {
-            const prices = response.data.subscription_plans.price_usd_per_gb
-            console.log(prices)
+            setPricingInfo(response.data.subscription_plans)
+            console.log(response.data.subscription_plans)
         })
         .catch(err => err)
+    }, [])
+
+    const findPrices = pricingInfo.filter(el => {
+        if (el.duration_months === props.planDuration) 
+        return el.price_usd_per_gb
     })
 
-    // const price1 = price.map(el => {
-    //     console.log(el)
-    // })
+    const monthlyPrice = findPrices.map(el => el.price_usd_per_gb)
 
-    
+    const showPrice = () => {
+        let price = 0;
+        if (props.planDuration === 3) {
+            price = props.gbVolume * monthlyPrice 
+        }
+        if (props.planDuration === 6) {
+            price = props.gbVolume * monthlyPrice 
+        }
+        if (props.planDuration === 12) {
+            price = props.gbVolume * monthlyPrice 
+        }
+        if (props.upfrontPayment === true) {
+            price = price*0.8
+        }
+        return price
+    }
 
     return (
         <div>
             <h1>{props.planDuration}</h1>
-            <h1>h</h1>
+            <h1>Monthly Price</h1>
+            <h1>{showPrice()}€</h1>
+            {props.upfrontPayment ?
+            <h1>{showPrice() * props.planDuration}€</h1>
+            : false}
         </div>
     )
 }
